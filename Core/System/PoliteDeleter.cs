64df9e91e328c9cdff55ac0ff2533d0fe696470f
mazbros <mazbros@gmail.com>
2016-03-16 17:25:35 -0400

    Added color for warning, fixed json

--- Core/System/PoliteDeleter.cs
+++ Core/System/PoliteDeleter.cs
@@ -18,9 +18,11 @@ namespace Qa.Core.System
             }
             catch (IOException)
             {
+                Console.ForegroundColor = ConsoleColor.Red;
                 Lo.Wl($"File {Path.GetFileName(path)} can't be deleted or recreated.");
                 Lo.Wl($"Please, close {path} file.");
                 Lo.Wl($"Preass any key to continue.");
+                Console.ResetColor();
                 Console.ReadKey();
                 Delete(path);
             }
