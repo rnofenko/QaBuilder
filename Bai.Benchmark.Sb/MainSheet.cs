d85da365547005e80d07718e759623f93d2211fc
mazbros <mazbros@gmail.com>
2016-03-24 14:36:12 -0400

    removed static methods

--- Bai.Benchmark.Sb/MainSheet.cs
+++ Bai.Benchmark.Sb/MainSheet.cs
@@ -14,7 +14,7 @@ namespace Qa.Bai.Benchmark.Sb
             new Header().Print(cursor, packet.Structure.Name);
             
             cursor.Column(InitColumn).Row(5);
-            Print.Content(packet, cursor);
+            new ContentGenerator().Print(packet, cursor, InitColumn);
             cursor.Down(2);
             
             sheet.Cells[sheet.Dimension.Address].AutoFitColumns();
